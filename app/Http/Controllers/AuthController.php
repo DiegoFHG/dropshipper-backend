6a7f6ba545e0075b8f1daf77ee582dd3b0ab4ecf
Diego Francisco Hernandez Guevara <34254918+DiegoFHG@users.noreply.github.com>
2021-06-01 00:34:13 +0200

    [fix] Fixed registration.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,19 +5,30 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller {
   public function register(Request $req) {
-    $req->validate([
+    $validations = Validator::make($req->all(), [
       'first_name' => 'required|string',
       'last_name' => 'required|string',
       'email' => 'required|email|unique:users',
       'password' => 'required|min:8'
     ]);
 
-    $user = User::create($req->all());
+    if ($validations->fails()) {
+      return response()->json($validations->errors(), 400);
+    } else {
+      $user = User::create([
+        'first_name' => $req->input('first_name'),
+        'last_name' => $req->input('last_name'),
+        'email' => $req->input('email'),
+        'password' => Hash::make($req->input('password'))
+      ]);
 
     return response()->json($user, 200);
+    }
   }
 
   public function login(Request $req) {
